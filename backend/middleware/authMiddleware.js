8ab638862161b817b926c5fe530242fbbc8a24db
kukur16jan <charitsharma660@gmail.com>
2025-08-05 20:54:46 +0530

    no back

--- backend/middleware/authMiddleware.js
+++
@@ -1,7 +0,0 @@
-// Authentication middleware to ensure user is logged in (for Express routes)
-module.exports = function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated && req.isAuthenticated()) {
-    return next();
-  }
-  return res.status(401).json({ message: 'Unauthorized' });
-};
